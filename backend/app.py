b2768b35877e9065ea33cc7acbf5ed680bcdb7db
kazuma <kazumamiyoshi0311@gmail.com>
2024-06-23 20:34:53 +0900

    backend commit

--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,2 @@
 from flask import Flask
-from flask_cors import CORS
-from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
-CORS(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-db = SQLAlchemy(app)
-
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
-
-if __name__ == '__main__':
-    app.run(debug=True)
